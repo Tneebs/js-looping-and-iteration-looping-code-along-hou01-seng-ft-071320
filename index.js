791e834af65875a5084070ed1a51cf7aa194e60e
Tneebs <tedneben@gmail.com>
2020-08-27 00:18:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+   const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(number) {
+    for ( let i = number; i >= 0; i -- ) {
+        console.log(i)
+    }
+}
